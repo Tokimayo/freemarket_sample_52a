28aa401c2281bd12d63a254467872a041621ac76
mryossy <life.is.beautifulmy@icloud.com>
2019-08-24 21:07:25 +0900

    tmp pass just number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
+  before_action :basic_auth 
   protect_from_forgery with: :exception
   private
 
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == "admin" && password == "aaaa"
     end
   end
 end
