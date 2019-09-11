4c81d6d23bc51ce559739f2b509989347ae94697
donkey96 <noire964@gmail.com>
2019-09-11 18:29:23 +0900

    サイズフォームの作成
    オートバイのサイズだけ実装したら挙動がおかしくなる時があったので保留にしてます(流石に必須では無いはずので)
    詳細はMTGで。

--- app/assets/javascripts/category_form.js
+++ app/assets/javascripts/category_form.js
@@ -1,57 +1,98 @@
-$(function () {
+$(document).on('turbolinks:load', function() {
+  $(function () {
 
-  function buildChild() {
-    var html =`<select class="select-wrap" id="child" name="item[category_id]"><option value="">---</option></select>`
-    return html;}
+    function buildChild() {
+      var html =`<select class="select-wrap" id="child" name="item[category_id]"><option value="">---</option></select>`
+      return html;}
 
-  function buildGrandChild() {
-    var html =`<select class="select-wrap2" id="grand_child" name="item[category_id]"><option value="">---</option></select>`
-    return html;}
-  
-  function buildOption(cateChild) {
-    var html = `<option value="${cateChild.id}">${cateChild.name}</option>` 
-    return html;}
+    function buildGrandChild() {
+      var html =`<select class="select-wrap2" id="grand_child" name="item[category_id]"><option value="">---</option></select>`
+      return html;}
+    
+    function buildOption(cateChild) {
+      var html = `<option value="${cateChild.id}">${cateChild.name}</option>` 
+      return html;}
 
-  $("#parent-form").on("change",function () {
-    $('#child').remove();
-    $('#grand_child').remove();
-    var parentValue = document.getElementById("parent-form").value;
-    $.ajax({
-      url: '/items/search',
-      type: "GET",
-      data: {parent_id: parentValue},
-      dataType: 'json'
-    })
-    .done(function (data) {
-      var html  = buildChild();
-      $('.data__category--form-m').append(html)
-      data.categories.forEach(function(cateChild) {
-        var option  = buildOption(cateChild);
-        $('#child').append(option);
+      function buildSize(){
+        var html = `<div class="data__size" id="sizebox">
+                      <label class="data__size--title">サイズ</label>
+                      <span class="required_mark">必須</span>
+                      <div class="data__size--tab"></div>
+                        <select class="select--box" required:"required" id="size" name="item[size_id]"></select>
+                      </div>
+                    </div>`
+
+        return html;}
+
+      function buildSizeOption(sizingOption){
+        var html = `<option value="${sizingOption.id}">${sizingOption.name}</option>`
+        return html;}
+
+    $("#parent-form").on("change",function () {
+      $('#child').remove();
+      $('#grand_child').remove();
+      $('#sizebox').remove();
+      var parentValue = document.getElementById("parent-form").value;
+      $.ajax({
+        url: '/items/search',
+        type: "GET",
+        data: {parent_id: parentValue},
+        dataType: 'json'
       })
-      $("#child").on("change",function () {
-        $('#grand_child').remove();
-        var parentValue = document.getElementById("child").value;
-        $.ajax({
-          url: '/items/search',
-          type: "GET",
-          data: {parent_id: parentValue},
-          dataType: 'json'
+      .done(function (data) {
+        var html  = buildChild();
+        $('.data__category--form-m').append(html)
+        data.categories.forEach(function(cateChild) {
+          var option  = buildOption(cateChild);
+          $('#child').append(option);
         })
-        .done(function (data) {
-          cnt = data.categories.length
-          if (cnt == 0) {
-            $('#grand_child').remove();
-          }else{
-          var html  = buildGrandChild();
-          $('.data__category--form-s').append(html)
-          data.categories.forEach(function(cateChild) {
-            var option  = buildOption(cateChild);
-            $('#grand_child').append(option);
+        $("#child").on("change",function () {
+          $('#grand_child').remove();
+          $('#sizebox').remove();
+          var parentValue = document.getElementById("child").value;
+          $.ajax({
+            url: '/items/search',
+            type: "GET",
+            data: {parent_id: parentValue},
+            dataType: 'json'
           })
-        }
-        })
-      })
-    })
-  })
+          .done(function (data) {
+            cnt = data.categories.length
+            if (cnt == 0) {
+              $('#grand_child').remove();
+            }else{
+            var html  = buildGrandChild();
+            $('.data__category--form-s').append(html)
+            data.categories.forEach(function(cateChild) {
+              var option  = buildOption(cateChild);
+              $('#grand_child').append(option);
+            })
+          }
+            $('#grand_child').on("change",function(){
+              $('#sizebox').remove();
+              var parentValue = document.getElementById("grand_child").value;
+              $.ajax({
+                  url:  '/items/search',
+                  type: "GET",
+                  data: { parent_id: parentValue },
+                  dataType: 'json'
+              })
+              .done(function(data) {
+                if(data.sizes == ""){$('#sizebox').css('display', 'none')
+                }else{
+                  $('.data__size').css('display', 'block')
+                    var html = buildSize();
+                  $('.data__size--form').append(html)
+                  data.sizes.forEach(function(sizingOption){
+                    var option = buildSizeOption(sizingOption);
+                    $('#size').append(option);
+                  });
+                }
+              });
+            });
+          });
+        });
+      });
+    });
+  });
 });
