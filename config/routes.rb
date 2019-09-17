0180b8636722d7bc2c8586238118d5228db337bb
Tokimayo <hayashiro05@gmail.com>
2019-09-18 07:06:25 +0900

    商品購入画面のマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,13 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  resources :buy, only: [:show] do
+    collection do
+      get 'purchase'
+      get 'done'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
 end
-
