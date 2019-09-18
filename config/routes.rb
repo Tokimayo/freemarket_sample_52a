30f74a3dec75ab68e737470503bcd73b7d9ba6c0
mryossy <life.is.beautifulmy@icloud.com>
2019-09-19 02:01:19 +0900

    sider修正

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
-  get 'users/:id' => 'users#show'
+  get 'users/:id', to: 'users#show'
 end
 
