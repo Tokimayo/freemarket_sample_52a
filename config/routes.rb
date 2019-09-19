2382ebe34312db40234a7499d6dfa72aaedae53e
mryossy <life.is.beautifulmy@icloud.com>
2019-09-20 05:59:15 +0900

    プロフィールパンくず作成

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   get 'users/:id', to: 'users#show'
+  get 'users/:id/profile', to: 'users#profile'
 end
 
