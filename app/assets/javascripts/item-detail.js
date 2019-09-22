4b00a1c947818d5a040a9f25a95e95dc0f6ae0fe
Tokimayo <hayashiro05@gmail.com>
2019-09-23 06:03:45 +0900

    いいねボタンのjquery　追加

--- app/assets/javascripts/item-detail.js
+++ app/assets/javascripts/item-detail.js
@@ -1,12 +1,29 @@
-window.onload = function(){
+$(document).on('turbolinks:load', function() {
 
   $(".item-detail__main-box--img-mini").hover(function(){
     var selectedImg = $(this).children(".item__detail--img-mini").attr('src');
     $(".item__detail--img").attr('src', selectedImg);
   });
 
+  
   $(".item-detail__btn--like").on("click", function(e){
     e.preventDefault();
-    $(".item-detail__btn--like").css("background-color", "white");
+
+    var likeBtnFlg = $(".item-detail__btn--like").data("flg");
+    console.log(likeBtnFlg);
+    $(".item-detail__btn--like").css("");
+    if( likeBtnFlg === "off" ){
+      $(".item-detail__btn--like").css("border", "thin solid #ef5185");
+      $(".item-detail__btn--like").css("background-color", "#fff");
+      $(".item-detail__btn--like").css("color", "#ef5185");
+      $(".item-detail__btn--like i").css("color", "#ef5185");
+      $(".item-detail__btn--like").data("flg", "on");
+    } else {
+      $(".item-detail__btn--like").css("border", "");
+      $(".item-detail__btn--like").css("background-color", "#f5f5f5");
+      $(".item-detail__btn--like").css("color", "");
+      $(".item-detail__btn--like i").css("color", "#ccc");
+      $(".item-detail__btn--like").data("flg", "off");
+    }
   });
-};
+});
